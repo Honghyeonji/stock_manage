af41c7b36a99f4f7f590b00d0f67955bef330015
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2022-11-20 03:44:23 +0900

    initial init

---
+++ stockmanage-spring/src/main/java/stockmanage/stockmanagespring/StockmanageSpringApplication.java
@@ -0,0 +1,13 @@
+package stockmanage.stockmanagespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmanageSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmanageSpringApplication.class, args);
+	}
+
+}
---
+++ stockmanage-spring/src/test/java/stockmanage/stockmanagespring/StockmanageSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package stockmanage.stockmanagespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmanageSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
